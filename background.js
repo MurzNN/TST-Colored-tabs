e92c5687e20f76be1af55c4f2948ad2490b86435
Alexey Murz Korepov <murznn@gmail.com>
2019-01-04 06:45:56 +0300

    First working version

--- background.js
+++ background.js
@@ -1,151 +1,250 @@
 "use strict";
 
 const TST_ID = "treestyletab@piro.sakura.ne.jp";
-const DEFAULT_SETTINGS = {
-};
+// const DEFAULT_SETTINGS = {
+// };
 
 // The current settings
-let settings;
-
-// Load settings, setup listeners and register to TST
-(async () => {
-    settings = await browser.storage.local.get(DEFAULT_SETTINGS);
-    await browser.storage.local.set(settings); // Save any missing defaults
-
-    browser.storage.onChanged.addListener(handleSettingsChange);
-    browser.runtime.onMessageExternal.addListener(handleTSTMessage);
-
-    // Register directly in case we are activated after TST
-    await registerToTST();
-})();
+// let settings;
 
 
-async function handleSettingsChange(changes) {
-    for(let key in changes) {
-        let change = changes[key];
-        if(change.newValue !== undefined) {
-            settings[key] = change.newValue;
+var ColoredTabs = {
+    init() {
+//        console.log("colorizeTab init");
+        browser.tabs.onUpdated.addListener(ColoredTabs.checkTabChanges);
+//         ColoredTabs.registerToTST();
+    },
+    
+    checkTabChanges(tabId, changeInfo, tab) {
+//       console.log("colorizeTab checkTabChanges tab id = " + tabId + " status " + changeInfo.status + " url " + changeInfo.url);
+            if (typeof changeInfo.url === 'undefined') {
+                return;
+            }
+//       console.log("colorizeTab checkTabChanges tab id = " + tabId + " url" );
+            let host = new URL(tab.url);
+            host = host.hostname.toString();
+            ColoredTabs.colorizeTab(tabId, host);
+    },
+    
+    colorizeTab(tabId, host, oldHost = null) {
+      
+      let hash = Math.abs(ColoredTabs.hostHash(host)) % 360;
+      
+//       let sat = await getOption("saturation");
+//       let lum = await getOption("lightness");
+      
+      if(typeof ColoredTabs.settings.hashColors[hash] === 'undefined') {
+      
+        ColoredTabs.settings.hashColors[hash] = true;
+        let hashColor = 'hsl(' + hash + ',' + ColoredTabs.settings.saturation + '%,' + ColoredTabs.settings.lightness + '%)';
+        let hashColorHover = 'hsl(' + hash + ',' + '100%,' + ColoredTabs.settings.lightness + '%)';
+        
+        ColoredTabs.settings.css = ColoredTabs.settings.css + `
+          .tab.coloredTabs` + hash + ` {
+            background-color: ` + hashColor + `;
+          }
+          .tab.coloredTabs` + hash + `:hover {
+            background-color: ` + hashColorHover + `;
+          }
+          `;
+
+        browser.runtime.sendMessage(TST_ID, {
+              type: "register-self",
+              style: ColoredTabs.settings.css,
+        });
+//         console.log(ColoredTabs.settings.hashColors);
+        console.log(ColoredTabs.settings.css);
+      }
+//       hash = Math.floor(Math.random() * (3 - 1 + 1)) + 1;
+      
+//       console.log("colorizeTab tab id = " + tabId + " host = " + host + " hash " + hash + " style coloredTabs" + hash);
+//       browser.runtime.sendMessage(TST_ID, {
+//         type:  'register-self',
+//         style: `
+//           .tab.coloredTabs` + hash + ` {
+//             color: red;
+//           }
+//         `
+//       });
+
+      browser.runtime.sendMessage(TST_ID, {
+        type:  'add-tab-state',
+        tabs:  [tabId],
+        state: 'coloredTabs' + hash,
+      });
+      
+    },
+    
+//     registerToTST() {
+//       try {
+//           const self = browser.management.getSelf();
+// //           browser.runtime.sendMessage(TST_ID, {
+// //               type: "register-self",
+// //               style: `
+// //                 .tab.style1 {
+// //                   background-color: red !important;
+// //                   border: 2px solid green !important;
+// //                 }
+// //                 .tab.style2 {
+// //                   background-color: blue !important;
+// //                   border: 5px solid yellow !important;
+// //                 }
+// //                 .tab.style3 {
+// //                   background-color: green !important;
+// //                   border: 5px solid blue !important;
+// //                 }
+// //               `
+// //           });
+//       } catch(e) {
+//           // Could not register
+//           console.log("Could not register to TST")
+//           return false;
+//       }
+// 
+//       return true;
+//   },
+  
+  hostHash(host) {
+        var hash = ColoredTabs.sha256(host);
+        var iClr, clrConst = 5381;
+        for (iClr = 0; iClr < hash.length; iClr++) {
+            clrConst = ((clrConst << 5) + clrConst) + hash.charCodeAt(iClr);
         }
-    }
-}
+        return clrConst;
+  },
+      // Generate a unique hash for a wider color spectrum
+    sha256(s) {
 
-async function registerToTST() {
-    try {
-        const self = await browser.management.getSelf();
-        await browser.runtime.sendMessage(TST_ID, {
-            type: "register-self",
-            name: self.id,
-            listeningTypes: ["ready", "tab-mouseover", "tab-mouseout"],
-              style: `
-                .tab.style1 {
-                  background-color: red !important;
-                  border: 2px solid green !important;
+        var chrsz = 8;
+        var hexcase = 0;
+
+        function safe_add(x, y) {
+            var lsw = (x & 0xFFFF) + (y & 0xFFFF);
+            var msw = (x >> 16) + (y >> 16) + (lsw >> 16);
+            return (msw << 16) | (lsw & 0xFFFF);
+        }
+
+        function S(X, n) {
+            return (X >>> n) | (X << (32 - n));
+        }
+
+        function R(X, n) {
+            return (X >>> n);
+        }
+
+        function Ch(x, y, z) {
+            return ((x & y) ^ ((~x) & z));
+        }
+
+        function Maj(x, y, z) {
+            return ((x & y) ^ (x & z) ^ (y & z));
+        }
+
+        function Sigma0256(x) {
+            return (S(x, 2) ^ S(x, 13) ^ S(x, 22));
+        }
+
+        function Sigma1256(x) {
+            return (S(x, 6) ^ S(x, 11) ^ S(x, 25));
+        }
+
+        function Gamma0256(x) {
+            return (S(x, 7) ^ S(x, 18) ^ R(x, 3));
+        }
+
+        function Gamma1256(x) {
+            return (S(x, 17) ^ S(x, 19) ^ R(x, 10));
+        }
+
+        function core_sha256(m, l) {
+            var K = new Array(0x428A2F98, 0x71374491, 0xB5C0FBCF, 0xE9B5DBA5, 0x3956C25B, 0x59F111F1, 0x923F82A4, 0xAB1C5ED5, 0xD807AA98, 0x12835B01, 0x243185BE, 0x550C7DC3, 0x72BE5D74, 0x80DEB1FE, 0x9BDC06A7, 0xC19BF174, 0xE49B69C1, 0xEFBE4786, 0xFC19DC6, 0x240CA1CC, 0x2DE92C6F, 0x4A7484AA, 0x5CB0A9DC, 0x76F988DA, 0x983E5152, 0xA831C66D, 0xB00327C8, 0xBF597FC7, 0xC6E00BF3, 0xD5A79147, 0x6CA6351, 0x14292967, 0x27B70A85, 0x2E1B2138, 0x4D2C6DFC, 0x53380D13, 0x650A7354, 0x766A0ABB, 0x81C2C92E, 0x92722C85, 0xA2BFE8A1, 0xA81A664B, 0xC24B8B70, 0xC76C51A3, 0xD192E819, 0xD6990624, 0xF40E3585, 0x106AA070, 0x19A4C116, 0x1E376C08, 0x2748774C, 0x34B0BCB5, 0x391C0CB3, 0x4ED8AA4A, 0x5B9CCA4F, 0x682E6FF3, 0x748F82EE, 0x78A5636F, 0x84C87814, 0x8CC70208, 0x90BEFFFA, 0xA4506CEB, 0xBEF9A3F7, 0xC67178F2);
+            var HASH = new Array(0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19);
+            var W = new Array(64);
+            var a, b, c, d, e, f, g, h, i, j;
+            var T1, T2;
+            m[l >> 5] |= 0x80 << (24 - l % 32);
+            m[((l + 64 >> 9) << 4) + 15] = l;
+            for (var i = 0; i < m.length; i += 16) {
+                a = HASH[0];
+                b = HASH[1];
+                c = HASH[2];
+                d = HASH[3];
+                e = HASH[4];
+                f = HASH[5];
+                g = HASH[6];
+                h = HASH[7];
+                for (var j = 0; j < 64; j++) {
+                    if (j < 16) W[j] = m[j + i];
+                    else W[j] = safe_add(safe_add(safe_add(Gamma1256(W[j - 2]), W[j - 7]), Gamma0256(W[j - 15])), W[j - 16]);
+                    T1 = safe_add(safe_add(safe_add(safe_add(h, Sigma1256(e)), Ch(e, f, g)), K[j]), W[j]);
+                    T2 = safe_add(Sigma0256(a), Maj(a, b, c));
+                    h = g;
+                    g = f;
+                    f = e;
+                    e = safe_add(d, T1);
+                    d = c;
+                    c = b;
+                    b = a;
+                    a = safe_add(T1, T2);
                 }
-                .tab.style2 {
-                  background-color: blue !important;
-                  border: 5px solid yellow !important;
+                HASH[0] = safe_add(a, HASH[0]);
+                HASH[1] = safe_add(b, HASH[1]);
+                HASH[2] = safe_add(c, HASH[2]);
+                HASH[3] = safe_add(d, HASH[3]);
+                HASH[4] = safe_add(e, HASH[4]);
+                HASH[5] = safe_add(f, HASH[5]);
+                HASH[6] = safe_add(g, HASH[6]);
+                HASH[7] = safe_add(h, HASH[7]);
+            }
+            return HASH;
+        }
+
+        function str2binb(str) {
+            var bin = Array();
+            var mask = (1 << chrsz) - 1;
+            for (var i = 0; i < str.length * chrsz; i += chrsz) {
+                bin[i >> 5] |= (str.charCodeAt(i / chrsz) & mask) << (24 - i % 32);
+            }
+            return bin;
+        }
+
+        function Utf8Encode(string) {
+            string = string.replace(/\r\n/g, "\n");
+            var utftext = "";
+            for (var n = 0; n < string.length; n++) {
+                var c = string.charCodeAt(n);
+                if (c < 128) {
+                    utftext += String.fromCharCode(c);
+                } else if ((c > 127) && (c < 2048)) {
+                    utftext += String.fromCharCode((c >> 6) | 192);
+                    utftext += String.fromCharCode((c & 63) | 128);
+                } else {
+                    utftext += String.fromCharCode((c >> 12) | 224);
+                    utftext += String.fromCharCode(((c >> 6) & 63) | 128);
+                    utftext += String.fromCharCode((c & 63) | 128);
                 }
-              `
-        });
-    } catch(e) {
-        // Could not register
-        console.log("Could not register to TST")
-        return false;
-    }
+            }
+            return utftext;
+        }
 
-    return true;
+        function binb2hex(binarray) {
+            var hex_tab = hexcase ? "0123456789ABCDEF" : "0123456789abcdef";
+            var str = "";
+            for (var i = 0; i < binarray.length * 4; i++) {
+                str += hex_tab.charAt((binarray[i >> 2] >> ((3 - i % 4) * 8 + 4)) & 0xF) +
+                    hex_tab.charAt((binarray[i >> 2] >> ((3 - i % 4) * 8)) & 0xF);
+            }
+            return str;
+        }
+        s = Utf8Encode(s);
+        return binb2hex(core_sha256(str2binb(s), s.length * chrsz));
+    },
+    
+    settings: {
+      'saturation': 60,
+      'lightness': 70,
+      'hashColors': {},
+    },
 }
 
-async function activateTab(tab) {
-    try {
-        await browser.runtime.sendMessage(TST_ID, {
-            type: "focus",
-            tab: tab
-        });
-    } catch(e) {
-        // Happens when the tab is closed during the timeout
-    }
-}
-
-
-let timer = null;
-function startTimer(tab) {
-    let callback = async () => {
-        timer = null;
-        await activateTab(tab);
-    };
-    timer = setTimeout(callback, settings["switching-delay"]);
-}
-function stopTimer() {
-    if(timer !== null) {
-        clearTimeout(timer);
-        timer = null;
-    }
-}
-
-async function handleTSTMessage(message, sender) {
-    if(sender.id !== TST_ID) {
-        return;
-    }
-// console.log(message.type);
-
-    switch (message.type) {
-        case "ready":
-            registerToTST();
-            
-            browser.runtime.sendMessage(TST_ID, {
-              type: 'register-self',
-            });
-
-            break;
-
-        case "tab-mouseover":
-//             stopTimer();
-//           console.log('tab-mouseover: yeaa 214');
-
-//             let tab = message.tab;
-//             console.log(tab.url);
-            console.log("tab-mouseover - URL: " + message.tab.url + " id " + message.tab.id);
-              // define colors
-            
-//             browser.runtime.sendMessage(TST_ID, {
-//               type: 'register-self',
-//               style: `
-//                 .tab.style1 {
-//                   background-color: red !important;
-//                   border: 2px solid green !important;
-//                 }
-//                 .tab.style2 {
-//                   background-color: blue !important;
-//                   border: 5px solid yellow !important;
-//                 }
-//               `
-//             });
-//         message.tab.style.background = 'yellow';
-//         message.tab.style.background = 'linear-gradient(to left, #ff0000 0%, #00ff00  100%)'; 
-//         getTabById(message.tab.id).style.background = 'green';
-//         getTabById(message.tab.id).style.background = `linear-gradient(to left, #ff0000 0%, #00ff00  100%)`; 
-if((message.tab.id % 2) == 1) {
-  browser.runtime.sendMessage(TST_ID, {
-    type:  'add-tab-state',
-    tabs:  [message.tab.id],
-    state: 'style1'
-  });
-} else {
-  browser.runtime.sendMessage(TST_ID, {
-    type:  'add-tab-state',
-    tabs:  [message.tab.id],
-    state: 'style2'
-  });
-}
-//   browser.runtime.sendMessage(kTST_ID, {
-//     type:  'add-tab-state',
-//     tabs:  [tab.id],
-//     state: 'style2'
-//   });            
-//             startTimer(tab.id);
-            console.log("tab-mouseover - URL: " + message.tab.url + " id " + message.tab.id + " FIN");
-            break;
-
-    }
-}
+ColoredTabs.init();
